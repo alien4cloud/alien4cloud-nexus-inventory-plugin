82e42f4dd6c89a443f12fc345c161e8a1585fcff
Sébastien LAURENT <sebastien.laurent@atos.net>
2020-09-03 16:06:37 +0200

    Initial Commit

---
+++ alien4cloud-back-nexus-inventory-plugin/src/main/java/org/alien4cloud/inventory/nexus/PluginConfiguration.java
@@ -0,0 +1,9 @@
+package org.alien4cloud.inventory.nexus;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "org.alien4cloud.inventory.nexus")
+public class PluginConfiguration {
+}
